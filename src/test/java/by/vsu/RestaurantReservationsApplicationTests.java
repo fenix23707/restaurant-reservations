1941dd0c3df8196235db6e19004434cca0b922aa
Vladislav Kovzov <fenix23707@gmail.com>
2021-11-03 10:09:26 +0300

    start project

---
+++ src/test/java/by/vsu/RestaurantReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.vsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

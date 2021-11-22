dc0b9e4c30a735e33ab6a3209c19c6427ef8be3f
Vladislav Kovzov <fenix23707@gmail.com>
2021-11-22 15:16:12 +0300

    add userInfoDao and tests for it

--- src/main/java/by/vsu/dao/RestaurantDao.java
+++ src/main/java/by/vsu/dao/RestaurantDao.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+
 public interface RestaurantDao {
     @Select("SELECT id, name, adres, avatar, user_id FROM RESTAURANTS")
     @ResultMap("restaurantResultMap")
---
+++ src/main/java/by/vsu/dao/UserInfoDao.java
@@ -0,0 +1,51 @@
+package by.vsu.dao;
+
+import by.vsu.model.User;
+import by.vsu.model.UserInfo;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+public interface UserInfoDao {
+    /**
+     * Find list of userInfo.
+     * @param limit number of userInfo to return.
+     * @param skip number of userInfo to skip.
+     * @return list of userInfo.
+     */
+    @Select("SELECT id, name, birthday, phone, avatar, email, user_id " +
+            "FROM user_info " +
+            "LIMIT #{limit} OFFSET #{skip} ")
+    @ResultMap("userInfoResultMap")
+    List<UserInfo> findAll(@Param("limit") int limit, @Param("skip") int skip);
+
+    /**
+     * Find userInfo by its id.
+     * @param id UserInfo id.
+     * @return UserInfo.
+     */
+    @Select("SELECT id, name, birthday, phone, avatar, email, user_id " +
+            "FROM user_info " +
+            "WHERE id = #{id}")
+    @ResultMap("userInfoResultMap")
+    UserInfo findById(@Param("id") Integer id);
+
+    /**
+     * Create new userInfo.UserInfo id should be auto-generated.
+     * Created UserInfo object saved in param userInfo.
+     * @param userInfo UserInfo data.
+     */
+    @Insert("INSERT INTO user_info (name, birthday, phone, avatar, email, user_id) " +
+            "VALUES(#{name}, #{birthday}, #{phone}, #{avatar}, #{email}, #{user.id})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void create(UserInfo userInfo);
+
+    /**
+     * Updates userInfo using given data.
+     * @param userInfo UserInfo data for update. Should have id set.
+     */
+    @Update("UPDATE user_info " +
+            "SET name = #{name}, birthday = #{birthday}, phone = #{phone}, avatar = #{avatar}, email = #{email}, user_id = #{user.id} " +
+            "WHERE id = #{id}")
+    void update(UserInfo userInfo);
+}
